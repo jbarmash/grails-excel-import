511005ca42a34f4562a17157cb01e2141d585b9b
Markus Menner <markus.menner@ananda-gmbh.de>
2014-05-22 15:13:59 +0200

    made Grails 2.4.0 compatible by changing ApplicationHolder, ConfigHolder to Holders

--- ExcelImportGrailsPlugin.groovy
+++ ExcelImportGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class ExcelImportGrailsPlugin {
     // the plugin version
-    def version = "1.0.0"
+    def version = "1.0.1"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.0.0 > *"
     // the other plugins this plugin depends on
