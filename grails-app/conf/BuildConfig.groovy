736fad52f86e93b9835f6b2947990d5c7ade5902
Omasirichukwu Udeinya <omasiri@novadge.com>
2015-02-02 17:26:00 +0100

    minor

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -38,7 +38,7 @@ grails.project.dependency.resolution = {
     }
 
     plugins {
-        build ':release:3.0.1', ':rest-client-builder:1.0.3'/*,":tomcat:7.0.53"*/,{
+        build ':release:3.0.1', ':rest-client-builder:1.0.3'/*,":tomcat:7.0.53"*/{
             export = false
         }
         //build ":tomcat:7.0.53"
