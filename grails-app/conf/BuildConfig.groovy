30cc832ff29b4b35069c68b7de322f04fc694e50
Jeff Scott Brown <jeff@jeffandbetsy.net>
2015-09-10 14:53:45 -0500

    Use Grails 2.4.5

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,48 +1,55 @@
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
-//grails.project.war.file = "target/${appName}-${appVersion}.war"
+
+grails.project.fork = [
+    // configure settings for compilation JVM, note that if you alter the Groovy version forked compilation is required
+    //  compile: [maxMemory: 256, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
+
+    // configure settings for the test-app JVM, uses the daemon by default
+    test: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
+    // configure settings for the run-app JVM
+    run: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
+    // configure settings for the run-war JVM
+    war: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
+    // configure settings for the Console UI JVM
+    console: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256]
+]
+
+grails.project.dependency.resolver = "maven" // or ivy
 grails.project.dependency.resolution = {
-	// inherit Grails' default dependencies
-	inherits("global") {
-		// uncomment to disable ehcache
-		// excludes 'ehcache'
-	}
+    // inherit Grails' default dependencies
+    inherits("global") {
+        // uncomment to disable ehcache
+        // excludes 'ehcache'
+    }
+    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+    repositories {
+        grailsCentral()
+        mavenLocal()
+        mavenCentral()
+        // uncomment the below to enable remote dependency resolution
+        // from public Maven repositories
+        //mavenRepo "http://repository.codehaus.org"
+        //mavenRepo "http://download.java.net/maven/2/"
+        //mavenRepo "http://repository.jboss.com/maven2/"
+    }
+    dependencies {
+	    // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
+	    compile(group: 'org.apache.poi', name: 'poi', version: '3.8');
+	//xlxs file support
+	    compile(group: 'org.apache.poi', name: 'poi-ooxml', version: '3.8') {
+	        excludes 'xmlbeans'
+	    }
+	    //compile group:'org.apache.poi', name:'poi-contrib', version:'3.7'
+	    //compile group:'org.apache.poi', name:'poi-scratchpad', version:'3.7' //ppt, word, visio, outlook support
+    }
 
-	log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
-	repositories {
-		grailsPlugins()
-		grailsHome()
-
-		// uncomment the below to enable remote dependency resolution
-		// from public Maven repositories
-		mavenLocal()
-		mavenCentral()
-		//mavenRepo "http://snapshots.repository.codehaus.org"
-		//mavenRepo "http://repository.codehaus.org"
-		//mavenRepo "http://download.java.net/maven/2/"
-		//mavenRepo "http://repository.jboss.com/maven2/"
-	}
-
-	dependencies {
-		// specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
-		compile(group: 'org.apache.poi', name: 'poi', version: '3.8');
-		//xlxs file support
-		compile(group: 'org.apache.poi', name: 'poi-ooxml', version: '3.8') {
-			excludes 'xmlbeans'
-		}
-		//compile group:'org.apache.poi', name:'poi-contrib', version:'3.7'
-		//compile group:'org.apache.poi', name:'poi-scratchpad', version:'3.7' //ppt, word, visio, outlook support
-	}
-
-	plugins {
-		build ":release:3.0.1"
-		build ":tomcat:7.0.53"
-
-		compile ':hibernate:3.6.10.15' {
-			export = false
-		}
-
+    plugins {
+        build(":release:3.1.0",
+              ":rest-client-builder:2.1.0") {
+            export = false
+        }
 		compile ':joda-time:1.5'
-	}
+    }
 }
