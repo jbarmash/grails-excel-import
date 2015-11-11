5e63ee30f67c0da4c786a7384427a60233acb4a3
Joseph Udeinya <omasiri@novadge.com>
2015-11-11 07:54:41 +0100

    upgraded master branch to grails 3x

--- src/main/groovy/ExcelImportGrailsPlugin.groovy
+++ src/main/groovy/ExcelImportGrailsPlugin.groovy
@@ -1,8 +1,14 @@
-class ExcelImportGrailsPlugin {
-    // the plugin version
-    def version = "1.0.1"
+package org.grails.plugins.excelImport
+
+import grails.plugins.*
+
+class ExcelImportGrailsPlugin extends Plugin {
+
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "2.0.0 > *"
+    def grailsVersion = "3.0.1 > *"
+    // resources that are excluded from plugin packaging
+    
+
     // the other plugins this plugin depends on
     def dependsOn = [:]
     // resources that are excluded from plugin packaging
@@ -31,30 +37,4 @@ class ExcelImportGrailsPlugin {
     // URL to the plugin's documentation
     def documentation = "http://grails.org/plugin/excel-import"
 
-    def doWithWebDescriptor = { xml ->
-        // TODO Implement additions to web.xml (optional), this event occurs before 
-    }
-
-    def doWithSpring = {
-        // TODO Implement runtime spring config (optional)
-    }
-
-    def doWithDynamicMethods = { ctx ->
-        // TODO Implement registering dynamic methods to classes (optional)
-    }
-
-    def doWithApplicationContext = { applicationContext ->
-        // TODO Implement post initialization spring config (optional)
-    }
-
-    def onChange = { event ->
-        // TODO Implement code that is executed when any artefact that this plugin is
-        // watching is modified and reloaded. The event contains: event.source,
-        // event.application, event.manager, event.ctx, and event.plugin.
-    }
-
-    def onConfigChange = { event ->
-        // TODO Implement code that is executed when the project configuration changes.
-        // The event is the same as for 'onChange'.
-    }
 }
